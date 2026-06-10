23a709144ac467f13b3c9ab8f4f2ef8beb97f514
prathipa2005 <lprathipa0@gmail.com>
2026-06-10 19:51:13 +0530

    initial commit

---
+++ class and  object/class_handle_copy.sv
@@ -0,0 +1,38 @@
+class mypkt;
+	int count;
+endclass
+
+module tb();
+	mypkt pkt,pkt1;
+
+	initial begin
+	
+		if(pkt==null)
+			$display("No object is created");
+
+		pkt=new();
+
+		if(pkt==null)
+			$display("Still obbject is not created");
+		else
+			$display("Object is created and pointing the property in the  class");
+
+		pkt.count=1;
+
+		$display("The value  of count is %0d",pkt.count);
+		pkt1=pkt;
+
+		$display("The value  of count is %0d",pkt.count);
+
+		$display("The value  of count is %0d",pkt1.count);
+
+		pkt.count=0;
+		
+		$display("The value  of count is %0d",pkt.count);
+		$display("The value  of count is %0d",pkt1.count);
+		pkt1.count=1;
+		$display("The value  of count is %0d",pkt.count);
+		$display("The value  of count is %0d",pkt1.count);
+	end
+	endmodule
+
