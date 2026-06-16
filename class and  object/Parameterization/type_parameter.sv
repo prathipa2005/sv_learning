eba112b8311808480073586f5bf9b011fe5a12e1
prathipa2005 <lprathipa0@gmail.com>
2026-06-16 19:01:53 +0530

    Day 4 : added Parameterized class

---
+++ class and  object/Parameterization/type_parameter.sv
@@ -0,0 +1,35 @@
+//***********************************************************************************************
+// File : Parameterized class .sv
+// Description : Demonstration of the Parameterized class by passing datatype
+//***********************************************************************************************
+
+
+
+class myclass #(type T = int);
+	T value;
+
+	function T addition (T a);
+		 addition= value + a;
+	endfunction
+
+endclass
+
+module tb;
+
+  myclass handle_1 =new();
+  myclass #(real)handle_2=new();
+  myclass #(bit[2:0]) handle_3=new;
+
+	initial begin
+      
+      
+		handle_1.value = 45;
+		handle_2.value = 4.23;
+		handle_3.value = 4'b1010;
+
+		$display("The value for handle_1 as int is %0d",handle_1.addition(21));
+      $display("The value for handle_2 as real is %0.2f",handle_2.addition(13));
+      $display("The value for handle_3 as 3 bit vector is %0d",handle_3.addition(8));
+
+	end
+	endmodule
